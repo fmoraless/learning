8a3a9315aab52b54a4ea3427283b51ffd3d88610
fmoraless <franticzero@gmail.com>
2019-01-06 23:48:00 -0300

    Usuarios, Roles, suscripciones y Socialite.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,17 +23,40 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->unsignedInteger('role_id')->default(\App\Role::STUDENT);
-            $table->foreign('role_id')->references('id')->on('users');
+            $table->foreign('role_id')->references('id')->on('roles');
             $table->string('name');
-            $table->string('email',128)->unique();
+            $table->string('email', 128)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('picture')->nullable();
+
+            //cashier columns
+            $table->string('stripe_id')->nullable();
+            $table->string('card_brand')->nullable();
+            $table->string('card_last_four')->nullable();
+            $table->string('trial_ends_at')->nullable();
+
             $table->rememberToken();
-
-
-
-
             $table->timestamps();
+        });
+        Schema::create('subscriptions', function ($table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('name');
+            $table->string('stripe_id')->collation('utf8mb4_bin');
+            $table->string('stripe_plan');
+            $table->integer('quantity');
+            $table->timestamp('trial_ends_at')->nullable();
+            $table->timestamp('ends_at')->nullable();
+            $table->timestamps();
+        });
+        Schema::create('user_social_accounts', function ($table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('provider');
+            $table->string('provider_uid');
         });
     }
 
@@ -46,5 +69,7 @@ class CreateUsersTable extends Migration
     {
         Schema::dropIfExists('users');
         Schema::dropIfExists('roles');
+        Schema::dropIfExists('subscriptions');
+        Schema::dropIfExists('user_social_accounts');
     }
 }
