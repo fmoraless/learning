0186671f18e1ffcf30aa2c383ac354f1a62a73f3
fmoraless <franticzero@gmail.com>
2019-01-09 18:02:48 -0300

    Autenticación para Facebook y GitHub.

--- config/services.php
+++ config/services.php
@@ -40,4 +40,15 @@ return [
         ],
     ],
 
+    'github' => [
+        'client_id' => env('GITHUB_CLIENT_ID'),
+        'client_secret' => env('GITHUB_CLIENT_SECRET'),
+        'redirect' => '/login/github/callback',
+    ],
+    'facebook' => [
+        'client_id' => env('FACEBOOK_CLIENT_ID'),
+        'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
+        'redirect' => '/login/facebook/callback',
+    ],
+
 ];
